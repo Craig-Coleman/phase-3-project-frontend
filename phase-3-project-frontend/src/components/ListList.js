f023e17256d1572779992c60837149fce1e512e4
Craig <craig.coleman.cc@gmail.com>
2022-06-13 15:23:52 -0500

    Completed assignment

--- phase-3-project-frontend/src/components/ListList.js
+++ phase-3-project-frontend/src/components/ListList.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function ListList({ lists }) {
 
+
    const listOfLists = lists.map(list => {
     return(
-        <li key={list.id}>{list.name}</li>
+        <li key={list.id}>
+            <NavLink className="navlink" to={`/lists/${list.id}`}>
+                {list.name}
+            </NavLink>
+        </li>
         )
-    }) 
+    })
+     
  
     return (
         <ul>
