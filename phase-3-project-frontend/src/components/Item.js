50054d94fdf42336d6c0fadd708193809bb6272b
Craig <craig.coleman.cc@gmail.com>
2022-06-14 12:21:46 -0500

    Create edit button that dynamically creates input field and submit button, and link input field to state

--- phase-3-project-frontend/src/components/Item.js
+++ phase-3-project-frontend/src/components/Item.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Item ({ item }) {
+
+    const { name, category, price } = item
+    const [itemName, setItemName] = useState(name)
+
+    function handleEditClick() {
+        if (document.getElementById("editDiv") === null) {
+        const editDiv = document.createElement('div')
+        editDiv.id = "editDiv"
+        const editForm = document.createElement('form');
+        editForm.addEventListener("submit", handleSubmit)
+        const editNameBox = document.createElement('input');
+        editNameBox.type = "text";
+        editNameBox.placeholder = "Edit Item Name";
+        editNameBox.id = "editNameBox"
+        editNameBox.addEventListener("change", handleEditName)
+        const editFormSubmitBtn = document.createElement('input');
+        editFormSubmitBtn.type = "submit";
+        editFormSubmitBtn.value = "Submit";
+        const item = document.getElementById("item");
+        item.appendChild(editDiv);
+        editDiv.appendChild(editForm);
+        editForm.appendChild(editNameBox);
+        editForm.appendChild(editFormSubmitBtn);
+        }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
+
+    function handleEditName(e) {
+        setItemName(e.target.value)
+    }
+
+    function handleDeleteClick() {
+        console.log("delete")
+    }
+
     return (
-        <div>
-            <h3>{item.name} / {item.category} / ${item.price}</h3>
+        <div id="item">
+            <h3>{itemName} / {category} / ${price}</h3>
+            <button onClick={handleEditClick}>edit name</button>
+            <select>
+                <option value="produce">Produce</option>
+                <option value="condiment">Condiment</option>
+                <option value="frozen">Frozen</option>
+                <option value="deli">Deli</option>
+            </select>
+            <button onClick={handleDeleteClick}>delete item</button>
         </div>
     );
 };
