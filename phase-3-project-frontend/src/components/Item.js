07ff5061078d35cbade382c9c6e88383d3576540
Craig <craig.coleman.cc@gmail.com>
2022-06-14 13:13:26 -0500

    Change from edit form creation upon click to permanent elements that are hidden, and unhidden when edit button clicked

--- phase-3-project-frontend/src/components/Item.js
+++ phase-3-project-frontend/src/components/Item.js
@@ -2,37 +2,43 @@ import React, { useState } from 'react';
 
 function Item ({ item }) {
 
-    const { name, category, price } = item
-    const [itemName, setItemName] = useState(name)
+    const { id, name, category, price } = item;
+
+    const [itemName, setItemName] = useState(name);
+    const [itemCategory, setItemCategory] = useState(category);
+    const [itemPrice, setItemPrice] = useState(price);
+
 
     function handleEditClick() {
-        if (document.getElementById("editDiv") === null) {
-        const editDiv = document.createElement('div')
-        editDiv.id = "editDiv"
-        const editForm = document.createElement('form');
-        editForm.addEventListener("submit", handleSubmit)
-        const editNameBox = document.createElement('input');
-        editNameBox.type = "text";
-        editNameBox.placeholder = "Edit Item Name";
-        editNameBox.id = "editNameBox"
-        editNameBox.addEventListener("change", handleEditName)
-        const editFormSubmitBtn = document.createElement('input');
-        editFormSubmitBtn.type = "submit";
-        editFormSubmitBtn.value = "Submit";
-        const item = document.getElementById("item");
-        item.appendChild(editDiv);
-        editDiv.appendChild(editForm);
-        editForm.appendChild(editNameBox);
-        editForm.appendChild(editFormSubmitBtn);
-        }
+        const editNameForm = document.getElementById(`editNameDiv${id}`);
+        const editPriceForm = document.getElementById(`editPriceDiv${id}`);
+        editNameForm.hidden = false;
+        editPriceForm.hidden = false;
+
     }
 
     function handleSubmit(e) {
         e.preventDefault()
+        const editNameDiv = document.getElementById("editNameDiv");
+        const editPriceDiv = document.getElementById("editPriceDiv");
+        fetch(`http://localhost:9292/items/${id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                name: itemName,
+                category: itemCategory,
+                price: itemPrice,
+            }),
+            headers: { 'Content-type': 'application/json' },
+        })
+        .then((res) => res.json());
     }
 
     function handleEditName(e) {
         setItemName(e.target.value)
+    }
+
+    function handleEditPrice(e) {
+        setItemPrice(e.target.value)
     }
 
     function handleDeleteClick() {
@@ -41,15 +47,27 @@ function Item ({ item }) {
 
     return (
         <div id="item">
-            <h3>{itemName} / {category} / ${price}</h3>
-            <button onClick={handleEditClick}>edit name</button>
-            <select>
+            <h3>{itemName} / {itemCategory} / ${itemPrice}</h3>
+            <button onClick={handleEditClick}>Edit Item</button>
+            <select selected={itemCategory}>
                 <option value="produce">Produce</option>
                 <option value="condiment">Condiment</option>
                 <option value="frozen">Frozen</option>
                 <option value="deli">Deli</option>
             </select>
             <button onClick={handleDeleteClick}>delete item</button>
+            <div id={`editNameDiv${id}`} hidden>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" placeholder="Enter New Name" onChange={(e) => handleEditName(e)}></input>
+                <input type="submit" value="save name"></input>
+                </form>
+            </div>
+            <div id={`editPriceDiv${id}`} hidden>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                <input type="text" placeholder="Enter New Price" onChange={(e) => handleEditPrice(e)}></input>
+                <input type="submit" value="save price"></input>
+                </form>
+            </div>
         </div>
     );
 };
